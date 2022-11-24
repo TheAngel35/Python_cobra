70906b787be939cc6fa8eab9154c2839e9ec6d6e
TheAngel35 <116230031+TheAngel35@users.noreply.github.com>
2022-11-24 01:13:36 +0000

    si

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from macarrao.Pessoa import Pessoa
+from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 fernandez= Pessoa(1,"Angel Santiago")
 print (fernandez)
+print("Daqui pra frente é acesso ao banco.")
+db = Database()
+pessoaDAO=PessoaDAO(db.conexao, db.cursor)
+pessoas= pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+  
